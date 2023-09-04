e50b291303782b5c603a88a1f6085576503e458b
dmitry.o <dmitry.o@appsflyer.com>
2023-09-04 15:57:13 +0300

    import fix

--- Plugins/iOS/AppsFlyerAdRevenueWrapper.h
+++ Plugins/iOS/AppsFlyerAdRevenueWrapper.h
@@ -7,7 +7,11 @@
 
 #import <Foundation/Foundation.h>
 
+#if __has_include(<AFUnityUitls.mm>)
 #import "AFUnityUtils.mm"
+#else
+#import "Libraries/appsflyer-unity-plugin/Plugins/iOS/AFUnityUtils.mm"
+#endif
 #if __has_include(<AppsFlyerAdRevenue/AppsFlyerAdRevenue.h>)
 #import <AppsFlyerAdRevenue/AppsFlyerAdRevenue.h>
 #endif
